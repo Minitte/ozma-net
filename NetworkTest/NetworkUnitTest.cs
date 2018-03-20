19f524696742a9ccc41d35faf497cafa217fc87a
Davis Pham <arcentalminitte@gmail.com>
2018-03-20 15:09:27 -0700

    unit test project

---
+++ NetworkTest/NetworkUnitTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using wpf_ozma_net.neural_network;
+
+namespace NetworkTest
+{
+    [TestClass]
+    public class NetworkUnitTest
+    {
+        [TestMethod]
+        public void TestNetworkSetup()
+        {
+            int[] layerSettings = { 1, 4, 1 };
+            Network network = new Network(layerSettings);
+        }
+    }
+}
