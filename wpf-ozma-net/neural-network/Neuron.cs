db8ac4f36c1e878bf6c74b4bfd6e80a6f9752ee8
Davis Pham <arcentalminitte@gmail.com>
2018-03-20 11:16:19 -0700

    neurons and it's layers

---
+++ wpf-ozma-net/neural-network/Neuron.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_ozma_net.neural_network
+{
+    /// <summary>
+    /// A class represneting a Neuron
+    /// </summary>
+    class Neuron
+    {
+        /// <summary>
+        /// list of connected neurons
+        /// </summary>
+        private Neuron[] m_connected;
+
+        /// <summary>
+        /// Weights for each connected neuron
+        /// </summary>
+        private float[] m_weights;
+
+        /// <summary>
+        /// list of connected neurons
+        /// </summary>
+        public Neuron[] Connected
+        {
+            get
+            {
+                return m_connected;
+            }
+        }
+
+        /// <summary>
+        /// Weights for each connected neuron
+        /// </summary>
+        public float[] Weights
+        {
+            get
+            {
+                return m_weights;
+            }
+        }
+
+        /// <summary>
+        /// Constructor for a neuron
+        /// </summary>
+        /// <param name="connected"></param>
+        /// <param name="weights"></param>
+        public Neuron(Neuron[] connected, float[] weights)
+        {
+            this.m_connected = connected;
+            this.m_weights = weights;
+        }
+    }
+}
---
+++ wpf-ozma-net/neural-network/NeuronLayer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_ozma_net.neural_network
+{
+    class NeuronLayer
+    {
+        /// <summary>
+        /// List of neurons in the layer
+        /// </summary>
+        private Neuron[] m_neurons;
+
+        /// <summary>
+        /// List of neurons in the layer
+        /// </summary>
+        public Neuron[] Neurons
+        {
+            get
+            {
+                return m_neurons;
+            }
+        }
+
+        public NeuronLayer(Neuron[] neurons)
+        {
+            this.m_neurons = neurons;
+        }
+    }
+}
