282603dc6eb5c85e5a375833e7afd5bbe5983af9
Minitte <arcentalminitte@gmail.com>
2018-03-20 21:13:50 -0700

    fixed unit test

--- NetworkTest/NetworkUnitTest.cs
+++ NetworkTest/NetworkUnitTest.cs
@@ -17,11 +17,11 @@ namespace NetworkTest
 
             NeuronLinkLayer[] layers = network.Layers;
 
-            Assert.AreEqual(layers.Length == layerSettings.Length, String.Format("Found {0} layers instead of {2}", layers.Length, layerSettings.Length));
+            Assert.AreEqual(layers.Length, layerSettings.Length, String.Format("Found {0} layers instead of {1}", layers.Length, layerSettings.Length));
 
             for (int i = 0; i < layerSettings.Length; i++)
             {
-                Assert.AreEqual(layers[i].Neurons.Length == layerSettings[i], String.Format("layer {0} has {1} neurons instead of {2}!", i, layers[i].Neurons.Length, layerSettings[i]));
+                Assert.AreEqual(layers[i].Neurons.Length, layerSettings[i], String.Format("layer {0} has {1} neurons instead of {2}!", i, layers[i].Neurons.Length, layerSettings[i]));
             }
         }
     }
