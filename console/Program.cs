3a7805f2947cfd6fe5ba748e91fafce0c3ba5c58
Minitte <arcentalminitte@gmail.com>
2018-03-20 21:14:08 -0700

    added console interface

---
+++ console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using ozmanet.neural_network;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] layerSettings = { 1, 4, 1 };
+            Network network = new Network(layerSettings);
+
+
+            Console.Write("Press any key to end");
+            Console.ReadKey();
+        }
+    }
+}
