a60aec73f93504066f695f17b3e36983f37b47fc
Davis Pham <arcentalminitte@gmail.com>
2018-03-26 15:25:12 -0700

    basic ui stuff

--- wpf-ozma-net/MainWindow.xaml.cs
+++ wpf-ozma-net/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Ink;
 
 namespace wpf_ozma_net
 {
@@ -20,9 +22,177 @@ namespace wpf_ozma_net
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private Stack<StrokeAction> m_undoStack;
+        private Stack<StrokeAction> m_redoStack;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            m_undoStack = new Stack<StrokeAction>();
+            m_redoStack = new Stack<StrokeAction>();
+        }
+
+        /// <summary>
+        /// Converts the drawing from the app into a byte array
+        /// </summary>
+        /// <returns></returns>
+        private byte[] DrawingToBitmap()
+        {
+            //get the dimensions of the ink control
+            int margin = (int)this.DrawingCanvas.Margin.Left;
+            int width = (int)this.DrawingCanvas.ActualWidth - margin;
+            int height = (int)this.DrawingCanvas.ActualHeight - margin;
+            //render ink to bitmap
+            RenderTargetBitmap rtb =
+            new RenderTargetBitmap(width, height, 96d, 96d, PixelFormats.Default);
+            rtb.Render(DrawingCanvas);
+            //save the ink to a memory stream
+            BmpBitmapEncoder encoder = new BmpBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(rtb));
+            byte[] bitmapBytes;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                encoder.Save(ms);
+                //get the bitmap bytes from the memory stream
+                ms.Position = 0;
+                bitmapBytes = ms.ToArray();
+            }
+            return bitmapBytes;
+        }
+
+        /// <summary>
+        /// Converts byte array into bitmapimage object
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public BitmapImage ToImage(byte[] array)
+        {
+            using (var ms = new System.IO.MemoryStream(array))
+            {
+                var image = new BitmapImage();
+                image.BeginInit();
+                image.CacheOption = BitmapCacheOption.OnLoad; // here
+                image.StreamSource = ms;
+                image.EndInit();
+                return image;
+            }
+        }
+
+        private void UpdateNetworkImage()
+        {
+
+            byte[] data = DrawingToBitmap();
+
+            BitmapImage img = ToImage(data);
+            WriteableBitmap modImg = new WriteableBitmap(img);
+
+            ScaleTransform s = new ScaleTransform(0.1, 0.1);
+
+            TransformedBitmap res = new TransformedBitmap(img, s);
+
+            NetworkImage.Source = res;
+        }
+
+        private void StrokeDrawnEvent(object sender, InkCanvasStrokeCollectedEventArgs e)
+        {
+            List<Stroke> strokeList = new List<Stroke>();
+            strokeList.Add(e.Stroke);
+            m_undoStack.Push(new StrokeAction(strokeList, true));
+
+            byte[] data = DrawingToBitmap();
+
+            m_redoStack.Clear();
+
+            UpdateNetworkImage();
+        }
+
+        private void BtnClearEvent(object sender, RoutedEventArgs e)
+        {
+            List<Stroke> strokeList = new List<Stroke>();
+
+            foreach (Stroke s in DrawingCanvas.Strokes)
+            {
+                strokeList.Add(s);
+            }
+
+            m_undoStack.Push(new StrokeAction(strokeList, false));
+
+            DrawingCanvas.Strokes.Clear();
+        }
+
+        private void UndoStroke()
+        {
+            if (m_undoStack.Count() != 0)
+            {
+                StrokeAction action = m_undoStack.Pop();
+
+                if (action.Add)
+                {
+                    foreach (Stroke s in action.StrokeList)
+                    {
+                        DrawingCanvas.Strokes.Remove(s);
+                    }
+                }
+                else {
+                    foreach (Stroke s in action.StrokeList)
+                    {
+                        DrawingCanvas.Strokes.Add(s);
+                    }
+                }
+
+                action.Add = !action.Add;
+                m_redoStack.Push(action);
+            }
+        }
+
+        private void RedoStroke()
+        {
+            if (m_redoStack.Count() != 0)
+            {
+                StrokeAction action = m_redoStack.Pop();
+
+                if (action.Add)
+                {
+                    foreach (Stroke s in action.StrokeList)
+                    {
+                        DrawingCanvas.Strokes.Remove(s);
+                    }
+                }
+                else
+                {
+                    foreach (Stroke s in action.StrokeList)
+                    {
+                        DrawingCanvas.Strokes.Add(s);
+                    }
+                }
+
+                action.Add = !action.Add;
+                m_undoStack.Push(action);
+            }
+        }
+
+        public class StrokeAction
+        {
+            public List<Stroke> StrokeList;
+            public bool Add;
+
+            public StrokeAction(List<Stroke> list, bool add)
+            {
+                this.Add = add;
+                this.StrokeList = list;
+            }
+        }
+
+        private void BtnUndoEvent(object sender, RoutedEventArgs e)
+        {
+            UndoStroke();
+        }
+
+        private void BtnRedoEvent(object sender, RoutedEventArgs e)
+        {
+            RedoStroke();
         }
     }
 }
