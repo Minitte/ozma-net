5fa1391436a5a71c6d777e102c1b3bda21316b36
Minitte <arcentalminitte@gmail.com>
2018-03-31 01:23:44 -0700

    copied .cs files from network
    Had to copy over the files since the project can't use referenced projects for some reason.
    
    Could not load file or assembly 'System.Runtime, Version=4.2.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a' or one of its dependencies. The system cannot find the file specified.

---
+++ wpf-ozma-net/util/MathF.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.CompilerServices;
+
+namespace ozmanet.util
+{
+    /// <summary>
+    /// A collection of math functions that uses floats instead of the standard doubles from c# library.
+    /// </summary>
+    public class MathF
+    {
+
+        public static double E = Math.E;
+
+        private MathF() { }
+
+        /// <summary>
+        /// Raises e raised to the power of value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static double Exp(double value)
+        {
+            return Math.Pow(Math.E, value);
+        }
+
+        /// <summary>
+        /// Sigmoid function
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static double Sigmoid(double value)
+        {
+            return 1.0 / (1.0 + Exp(-value));
+        }
+
+        /// <summary>
+        /// Sigmoid prime function
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static double SigmoidPrimes(double value)
+        {
+            return Sigmoid(value) * (1.0 - Sigmoid(value));
+        }
+
+
+    }
+}
