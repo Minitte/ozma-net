11168547f1f2c14db95e2a5308c2ec789ddf281b
Minitte <arcentalminitte@gmail.com>
2018-04-10 23:04:23 -0700

    moved writer to creator project

---
+++ mnist_data_creator/GeneralImage.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mnist_data_creator
+{
+    public class GeneralImage
+    {
+        /// <summary>
+        /// image data
+        /// </summary>
+        private byte[,] m_image;
+
+        /// <summary>
+        /// label for the image
+        /// </summary>
+        private string m_label;
+
+        /// <summary>
+        /// label index or output index
+        /// </summary>
+        private byte m_labelIndex;
+
+        /// <summary>
+        /// Image data
+        /// </summary>
+        public byte[,] Image
+        {
+            get
+            {
+                return m_image;
+            }
+
+            set
+            {
+                m_image = value;
+            }
+        }
+
+        /// <summary>
+        /// Label for the image
+        /// </summary>
+        public string Label
+        {
+            get
+            {
+                return Label;
+            }
+
+            set
+            {
+                m_label = value;
+            }
+        }
+
+        /// <summary>
+        /// label index or output index
+        /// </summary>
+        public byte LabelIndex
+        {
+            get
+            {
+                return m_labelIndex;
+            }
+
+            set
+            {
+                m_labelIndex = value;
+            }
+        }
+
+        public GeneralImage(byte[,] image, string label)
+        {
+            m_image = image;
+            m_label = label;
+        }
+
+        public GeneralImage(byte[,] image, byte labelIndex)
+        {
+            m_image = image;
+            m_labelIndex = labelIndex;
+        }
+    }
+}
--- mnist_data_creator/MnistLabelWriter.cs
+++ mnist_data_creator/MnistLabelWriter.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 using System.IO;
 
-namespace ozmanet.util
+namespace mnist_data_creator
 {
     public class MnistLabelWriter
     {
